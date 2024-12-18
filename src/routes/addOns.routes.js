36b095917d695387bd7076b9236b3ecb5aa4ff21
Sonu Kumar <thesonukumar357@gmail.com>
2024-12-18 17:12:26 +0530

    Middelware Added

--- src/routes/addOns.routes.js
+++ src/routes/addOns.routes.js
@@ -1,13 +1,14 @@
 import express from 'express';
-
+import {isAdmin,isAuthenticated} from '../middleware/authenticateUser.js'
 import {createAddOn, getAllAddOns, getAddOnById, updateAddOn, deleteAddOn} from '../controllers/addons/addOns.controller.js';
 
 const addOnsRouter = express.Router();
 
-addOnsRouter.post('/create-addons', createAddOn);
-addOnsRouter.get('/get-addons', getAllAddOns);
-addOnsRouter.get('/get-addons/:id', getAddOnById);
+
+addOnsRouter.post('/create-addons',isAdmin,isAuthenticated, createAddOn);
+addOnsRouter.get('/get-addons',isAdmin,isAuthenticated, getAllAddOns);
+addOnsRouter.get('/get-addons/:id',isAdmin,isAuthenticated, getAddOnById);
 addOnsRouter.put('/update-addons:id', updateAddOn);
-addOnsRouter.delete('/remove-addons/:id', deleteAddOn);
+addOnsRouter.delete('/remove-addons/:id',isAdmin,isAuthenticated, deleteAddOn);
 
 export default addOnsRouter;
