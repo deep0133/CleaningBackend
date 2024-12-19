01bf489ac84d7780546cb3e2460ff38bde60057e
deep0133 <deepanshuheer27@gmail.com>
2024-12-19 12:28:01 +0530

    addons api tested

--- src/routes/addOns.routes.js
+++ src/routes/addOns.routes.js
@@ -1,14 +1,24 @@
-import express from 'express';
-import {isAdmin,isAuthenticated} from '../middleware/authenticateUser.js'
-import {createAddOn, getAllAddOns, getAddOnById, updateAddOn, deleteAddOn} from '../controllers/addons/addOns.controller.js';
+import express from "express";
+import { isAdmin, isAuthenticated } from "../middleware/authenticateUser.js";
+import {
+  createAddOn,
+  getAllAddOns,
+  getAddOnById,
+  updateAddOn,
+  deleteAddOn,
+} from "../controllers/addons/addOns.controller.js";
 
 const addOnsRouter = express.Router();
 
-
-addOnsRouter.post('/create-addons',isAdmin,isAuthenticated, createAddOn);
-addOnsRouter.get('/get-addons',isAdmin,isAuthenticated, getAllAddOns);
-addOnsRouter.get('/get-addons/:id',isAdmin,isAuthenticated, getAddOnById);
-addOnsRouter.put('/update-addons:id', updateAddOn);
-addOnsRouter.delete('/remove-addons/:id',isAdmin,isAuthenticated, deleteAddOn);
+addOnsRouter.post("/create-addons", isAuthenticated, isAdmin, createAddOn);
+addOnsRouter.get("/get-addons", isAuthenticated, getAllAddOns);
+addOnsRouter.get("/get-addons/:id", getAddOnById);
+addOnsRouter.put("/update-addons/:id", isAuthenticated, isAdmin, updateAddOn);
+addOnsRouter.delete(
+  "/remove-addons/:id",
+  isAuthenticated,
+  isAdmin,
+  deleteAddOn
+);
 
 export default addOnsRouter;
