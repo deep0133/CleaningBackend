7303fe71ea2e5c2713597bdedc896cbea0a35f75
Gaurav Kandhalia <vaidkandhala333@gmail.com>
2024-12-30 16:29:43 +0530

    added dummy data line deleted

--- src/app.js
+++ src/app.js
@@ -8,10 +8,10 @@ import addOnsRouter from "./routes/addOns.routes.js";
 import cartRouter from "./routes/cart.routes.js";
 import { verifyStripePayment } from "./controllers/payment/verifyPaymentWebhook.js";
 import { balanceWebhook } from "./controllers/payment/balanceWebhook.js";
-import {
-  addDummyData,
-  getNearbyDummyUsers,
-} from "./controllers/user/user.controller.js";
+// import {
+
+//   getNearbyDummyUsers,
+// } from "./controllers/user/user.controller.js";
 
 const app = express();
 
@@ -37,8 +37,8 @@ app.use(
 app.use("/images", express.static("uploads"));
 
 // app.get("/", findNearbyCleaner);
-app.get("/addDummyData", addDummyData);
-app.post("/getNearbyDummyUsers", getNearbyDummyUsers);
+// app.get("/addDummyData", addDummyData);
+// app.post("/getNearbyDummyUsers", getNearbyDummyUsers);
 
 app.use("/api/v1/users", userRouter);
 app.use("/api/v1/otp", otpRouter);
