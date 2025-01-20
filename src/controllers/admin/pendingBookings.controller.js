0bb7aee13a6b7e1b6ce89c9094c3e05b9c5e141c
Gaurav Kandhalia <vaidkandhala333@gmail.com>
2025-01-20 14:48:37 +0530

    name of the file changed

--- src/controllers/admin/pendingBookings.controller.js
+++ src/controllers/admin/pendingBookings.controller.js
@@ -1,7 +1,7 @@
 import { BookingService } from "../../models/Client/booking.model.js";
 import {asyncHandler} from '../../utils/asyncHandler.js'
-import {apiResponse} from '../../utils/apiResponse.js'
-import {apiError} from '../../utils/apiError.js'
+import {ApiResponse} from '../../utils/apiResponse.js'
+
 export const pendingBookings = asyncHandler(async (req,res)=>{
     const currentTime = new Date()
     const twoHoursAgo = new Date(currentTime.getTime() - 2 * 60 * 60 * 1000);
@@ -15,10 +15,10 @@ export const pendingBookings = asyncHandler(async (req,res)=>{
 
     if(!bookings){
         res.status(201)
-        .json(new apiResponse(201,{},"no pending Bookings",true))
+        .json(new ApiResponse(201,{},"no pending Bookings",true))
     }
 
     res.status(201)
-    .json(new apiResponse(201,bookings,"Bookings Still pending",true));
+    .json(new ApiResponse(201,bookings,"Bookings Still pending",true));
 
 })
